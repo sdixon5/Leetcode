6c83de0040570c9a1f9f0a2b3d6a83ae3eea3941
Shawn Dixon <shawn12dixon@gmail.com>
2025-06-25 17:48:19 -0600

    End of 485
    Didn't copy text from editorial piece.

---
+++ Arrays101/485/MaxConsecutiveOnes/MaxConsecutiveOnes/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace MaxConsecutiveOnes
+{
+    public class Solution
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            int consecutiveCount = 0;
+
+            int maxCount = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 1)
+                {
+                    consecutiveCount++;
+                }
+                else
+                {
+                    maxCount = Math.Max(maxCount, consecutiveCount);
+                    consecutiveCount = 0;
+                }
+            }
+
+            // Final check in case the array ends with 1s
+            maxCount = Math.Max(maxCount, consecutiveCount);
+
+            return maxCount;
+        }
+    }
+}
